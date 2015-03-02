f800ef6307edfbf24d06ed82fa1a99a43efb2bc1
hydren <5carlosfelipe5@gmail.com>
2015-03-01 21:41:20 -0300

    Created util.hpp and EuclideanVector class.

---
+++ src/util.hpp
@@ -0,0 +1,18 @@
+/*
+ * util.hpp
+ *
+ *  Created on: 01/03/2015
+ *      Author: felipe
+ */
+
+#ifndef UTIL_HPP_
+#define UTIL_HPP_
+
+#include <string>
+using std::string;
+
+/* Useful defines */
+
+#define abstract =0;
+
+#endif /* UTIL_HPP_ */
