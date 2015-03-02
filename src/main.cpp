d95aac753eca69518c0351fb2f5b4905f0e6d73f
hydren <5carlosfelipe5@gmail.com>
2015-03-02 17:11:35 -0300

    Added main function.

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 02/03/2015
+ *      Author: Carlos F. M. Faruolo <5carlosfelipe5@gmail.com>
+ */
+
+#include <cstdlib>
+
+int main(int arc, char* argv[])
+{
+	return EXIT_SUCCESS;
+}
+
+
