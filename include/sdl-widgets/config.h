8a825ff8a1df63e85da4344d3b595e3bb86fe2f7
hydren <5carlosfelipe5@gmail.com>
2015-06-19 14:47:10 -0300

    Added win32-specific fonts path. This should be temporary as it should embbed a font.

--- include/sdl-widgets/config.h
+++ include/sdl-widgets/config.h
@@ -8,8 +8,20 @@
 #ifndef SDL_WIDGETS_CONFIG_H_
 #define SDL_WIDGETS_CONFIG_H_
 
+//Replace this for a proper configuration file (or remove it altogether)
+
+#ifdef _WIN32
+const char* FONTPATH="C:\\Windows\\Fonts\\DejaVuSans.ttf";
+const char* FONTPATH_BOLD="C:\\Windows\\Fonts\\DejaVuSans-Bold.ttf";
+const char* FONTPATH_MONO="C:\\Windows\\Fonts\\DejaVuSans-Bold.ttf";
+#endif
+
+#ifndef _WIN32
+
 const char* FONTPATH="/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf";
 const char* FONTPATH_BOLD="/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Bold.ttf";
 const char* FONTPATH_MONO="/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Bold.ttf";
+
+#endif
 
 #endif /* SDL_WIDGETS_CONFIG_H_ */
