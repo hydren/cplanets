d9970306d04704d40047f65a681a260614f8a5f5
hydren <5carlosfelipe5@gmail.com>
2015-06-19 16:19:50 -0300

    Moved SDL Widgets example to its own file.

---
+++ include/sdl-widgets/example.hpp
@@ -0,0 +1,15 @@
+/*
+ * example.hpp
+ *
+ *  Created on: 19/06/2015
+ *      Author: Visagio
+ */
+
+#ifndef SDL_WIDGETS_EXAMPLE_HPP_
+#define SDL_WIDGETS_EXAMPLE_HPP_
+
+#include "sdl-widgets/sdl-widgets.h"
+
+void Widgets_RunExample();
+
+#endif /* SDL_WIDGETS_EXAMPLE_HPP_ */
