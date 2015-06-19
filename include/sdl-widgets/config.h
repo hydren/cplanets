b3376f4a09b2200ee127a4c8586ab70f0fed1430
hydren <5carlosfelipe5@gmail.com>
2015-06-19 14:56:44 -0300

    Refactoring of config file.

--- include/sdl-widgets/config.h
+++ include/sdl-widgets/config.h
@@ -11,17 +11,13 @@
 //Replace this for a proper configuration file (or remove it altogether)
 
 #ifdef _WIN32
-const char* FONTPATH="C:\\Windows\\Fonts\\DejaVuSans.ttf";
-const char* FONTPATH_BOLD="C:\\Windows\\Fonts\\DejaVuSans-Bold.ttf";
-const char* FONTPATH_MONO="C:\\Windows\\Fonts\\DejaVuSans-Bold.ttf";
-#endif
-
-#ifndef _WIN32
-
-const char* FONTPATH="/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf";
-const char* FONTPATH_BOLD="/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Bold.ttf";
-const char* FONTPATH_MONO="/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Bold.ttf";
-
+	const char* FONTPATH="C:\\Windows\\Fonts\\DejaVuSans.ttf";
+	const char* FONTPATH_BOLD="C:\\Windows\\Fonts\\DejaVuSans-Bold.ttf";
+	const char* FONTPATH_MONO="C:\\Windows\\Fonts\\DejaVuSans-Bold.ttf";
+#else
+	const char* FONTPATH="/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf";
+	const char* FONTPATH_BOLD="/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Bold.ttf";
+	const char* FONTPATH_MONO="/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Bold.ttf";
 #endif
 
 #endif /* SDL_WIDGETS_CONFIG_H_ */
