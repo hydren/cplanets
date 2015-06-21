8504a23953856df6f0fb9472d3c0dd646204a97d
hydren <5carlosfelipe5@gmail.com>
2015-06-21 15:43:40 -0300

    Added preliminar physics2d class

---
+++ include/physics/physics2d.hpp
@@ -0,0 +1,21 @@
+/*
+ * physics2d.hpp
+ *
+ *  Created on: 20/06/2015
+ *      Author: Visagio
+ */
+
+#ifndef PHYSICS_PHYSICS2D_HPP_
+#define PHYSICS_PHYSICS2D_HPP_
+
+#include "universe2d.hpp"
+
+struct Physics2D
+{
+	Universe2D universe;
+
+
+};
+
+
+#endif /* PHYSICS_PHYSICS2D_HPP_ */
