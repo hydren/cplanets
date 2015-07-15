689be01d2b0d184d925278c080a090d57d2e02ad
hydren <5carlosfelipe5@gmail.com>
2015-07-15 15:13:34 -0300

    Added const_foreach macro

--- include/futil/futil.hpp
+++ include/futil/futil.hpp
@@ -41,6 +41,11 @@ for(COLLECTION_TYPE::iterator ELEMENT##MACRO_TEMP_IT = (COLLECTION).begin(); ELE
 for(bool ELEMENT##MACRO_TEMP_B = true; ELEMENT##MACRO_TEMP_B;)\
 for(TYPE ELEMENT = *(ELEMENT##MACRO_TEMP_IT); ELEMENT##MACRO_TEMP_B; ELEMENT##MACRO_TEMP_B = false)
 
+#define const_foreach(TYPE, ELEMENT, COLLECTION_TYPE, COLLECTION)\
+for(COLLECTION_TYPE::const_iterator ELEMENT##MACRO_TEMP_IT = (COLLECTION).begin(); ELEMENT##MACRO_TEMP_IT != (COLLECTION).end(); ++ELEMENT##MACRO_TEMP_IT)\
+for(bool ELEMENT##MACRO_TEMP_B = true; ELEMENT##MACRO_TEMP_B;)\
+for(TYPE ELEMENT = *(ELEMENT##MACRO_TEMP_IT); ELEMENT##MACRO_TEMP_B; ELEMENT##MACRO_TEMP_B = false)
+
 #include <iostream>
 typedef std::ifstream FileInputStream;
 typedef std::ofstream FileOutputStream;
