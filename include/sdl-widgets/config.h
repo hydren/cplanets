2405eaa2fa8d1c5044bd3fc3f5d43e11f4ef15fa
Hydren <5carlosfelipe5@gmail.com>
2015-11-23 15:28:18 -0200

    Updated default fonts paths.

--- include/sdl-widgets/config.h
+++ include/sdl-widgets/config.h
@@ -11,13 +11,13 @@
 //Replace this for a proper configuration file (or remove it altogether)
 
 #ifdef _WIN32
-	const char* FONTPATH="C:\\Windows\\Fonts\\DejaVuSans.ttf";
-	const char* FONTPATH_BOLD="C:\\Windows\\Fonts\\DejaVuSans-Bold.ttf";
-	const char* FONTPATH_MONO="C:\\Windows\\Fonts\\DejaVuSans-Bold.ttf";
+	const char* FONTPATH="C:\\Windows\\Fonts\\FreeSans.ttf";
+	const char* FONTPATH_BOLD="C:\\Windows\\Fonts\\FreeSansBold.ttf";
+	const char* FONTPATH_MONO="C:\\Windows\\Fonts\\FreeMono.ttf";
 #else
-	const char* FONTPATH="/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf";
-	const char* FONTPATH_BOLD="/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Bold.ttf";
-	const char* FONTPATH_MONO="/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Bold.ttf";
+	const char* FONTPATH="/usr/share/fonts/truetype/freefont/FreeSans.ttf";
+	const char* FONTPATH_BOLD="/usr/share/fonts/truetype/freefont/FreeSansBold.ttf";
+	const char* FONTPATH_MONO="/usr/share/fonts/truetype/freefont/FreeMono.ttf";
 #endif
 
 #endif /* SDL_WIDGETS_CONFIG_H_ */
