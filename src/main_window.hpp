12ca7ac5326f24ec92fad4bab081871e2fcb5914
Carlos Faruolo <5carlosfelipe5@gmail.com>
2016-03-25 02:16:50 -0300

    Added comments

--- src/main_window.hpp
+++ src/main_window.hpp
@@ -15,16 +15,26 @@
 //needed to display the version
 extern const std::string CPLANETS_VERSION;
 
+//contains all cplanets gui related stuff
 namespace CPlanetsGUI
 {
+	//stuff related to the main window
 	namespace MainWindow
 	{
+		//show the main window and starts all its events treatments
 		void show();
 	}
 
+	//convert the given sdl color to an int color suitable to be used on the given surface
 	int colorToInt(const SDL_Surface* surf, const SDL_Color& color, bool forceRGBA=false);
+
+	//set the color components of the given sdl color. use -1 to leave the component unchanged (i.e. modifyColor(-1,224,-1) only sets the green component to 224)
 	void modifyColor(SDL_Color& color, int r, int g, int b);
+
+	//creates a random color
 	SDL_Color* getRandomColor();
+
+	//trigger repainting of the whole window. it will be repainted eventually
 	void triggerRepaint();
 }
 
