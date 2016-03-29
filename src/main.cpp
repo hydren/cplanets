a371e7890efdd7d37a3dc9052780d0a8022afecb
Carlos F. M. Faruolo <5carlosfelipe5@gmail.com>
2016-03-29 14:03:51 -0300

    Bumped version

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@
 #include "geometry/vector2d.hpp"
 #include "main_window.hpp"
 
-const string CPLANETS_VERSION = "0.1.2-alpha";
+const string CPLANETS_VERSION = "0.1.3-alpha";
 
 int main(int arc, char* argv[])
 {
