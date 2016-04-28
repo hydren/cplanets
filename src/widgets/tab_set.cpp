6eb5de1d31ff57899e2f944487ee44cfd2b12e59
Carlos F. M. Faruolo <5carlosfelipe5@gmail.com>
2016-04-28 15:46:44 -0300

    Created TabSet, a layout that manages "tab" RExtButton creation/position
    Extending SDL_util::FlowLayout, this struct manages button creation (but
    not removal/deletion yet) and position using FlowLayout methods. It's
    still doesn't manages resize events automatically.

---
+++ src/widgets/tab_set.cpp
@@ -0,0 +1,39 @@
+/*
+ * tab_set.cpp
+ *
+ *  Created on: 28 de abr de 2016
+ *      Author: carlosfaruolo
+ */
+
+#include "tab_set.hpp"
+
+#include "widgets_util.hpp"
+#include "futil/futil.hpp"
+
+using SDL_util::TabSet;
+using SDL_util::FlowLayout;
+
+TabSet::TabSet(WinBase* parent, int x, int y, Rect maxSize)
+: FlowLayout(x, y, maxSize),
+  controller(parent)
+{}
+
+void TabSet::addTab(Label lab, BgrWin* content)
+{
+	controller.addTab(Rect(0,0,1,1), lab, content);
+	RExtButton* tabBtn = controller.tabButtons.back();
+	SDL_util::packLabeledComponent(tabBtn);
+	addComponent(tabBtn);
+	pack();
+}
+
+void TabSet::setActiveTab(BgrWin* tabContent)
+{
+	controller.setActiveTab(tabContent);
+}
+
+void TabSet::setActiveTab(unsigned index)
+{
+	controller.setActiveTab(index);
+}
+
---
+++ src/widgets/tab_set.hpp
@@ -0,0 +1,37 @@
+/*
+ * tab_set.hpp
+ *
+ *  Created on: 28 de abr de 2016
+ *      Author: carlosfaruolo
+ */
+
+#ifndef WIDGETS_TAB_SET_HPP_
+#define WIDGETS_TAB_SET_HPP_
+
+#include "SDL_widgets/SDL_widgets.h"
+#include "futil/futil.hpp"
+#include "flow_layout.hpp"
+#include "tab_control.hpp"
+
+namespace SDL_util
+{
+	/// A set of tab buttons arranged together. It also contains an inner tab controller.
+	struct TabSet extends FlowLayout
+	{
+		TabController controller;
+
+		/// Creates a TabSet on the specified position. See FlowLayout for more info on its behavior.
+		TabSet(WinBase* parent, int x, int y, Rect maxSize=Rect(-1,-1,0,0));
+
+		/// Adds a new tab with the given label and content.
+		void addTab(Label lab, BgrWin* content);
+
+		/// Sets the active (visible) tab.
+		void setActiveTab(unsigned index);
+		void setActiveTab(BgrWin* tabContent);
+	};
+}
+
+
+
+#endif /* WIDGETS_TAB_SET_HPP_ */
