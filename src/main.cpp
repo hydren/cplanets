77879bf299b6aed130eaaee70df7145627a76a93
Carlos Faruolo <5carlosfelipe5@gmail.com>
2016-05-11 01:17:27 -0300

    Bumped version. Updated reference.

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@ using std::string;
 using std::exception;
 using std::cout; using std::endl;
 
-const string CPLANETS_VERSION = "0.4.5-beta";
+const string CPLANETS_VERSION = "0.4.6-beta";
 
 const string CPLANETS_LICENSE =
 		"Copyright (C) 2016 Carlos F. M. Faruolo\n"
