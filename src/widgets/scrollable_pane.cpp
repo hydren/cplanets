60302386e4fa7c91814174031634562760cca2c0
Carlos F. M. Faruolo <5carlosfelipe5@gmail.com>
2016-06-21 16:07:16 -0300

    Extracted refreshing functionality of updateOffset() to refresh()

--- src/widgets/scrollable_pane.cpp
+++ src/widgets/scrollable_pane.cpp
@@ -29,21 +29,18 @@ void ScrollablePane::draw()
 	SDL_FillRect(this->win, null, this->bgcol); //clears the screen
 }
 
-#include <iostream>
+void ScrollablePane::refresh()
+{
+	this->draw_blit_recur();
+	this->hide(); //xxx cumbersome workaround for ScrollablePane::refresh()
+	this->show(); //cumbersome workaround for ScrollablePane::refresh()
+}
 
 void ScrollablePane::updateOffset(int dx, int dy)
 {
 	offset.x += dx; offset.y += dy;
-
 	content.move(dx, dy);
-	this->draw_blit_recur();
-	this->hide(); //xxx cumbersome workaround for ScrollablePane::updateOffset()
-	this->show(); //cumbersome workaround for ScrollablePane::updateOffset()
-
-	// DEBUG
-//	std::cout << "dx=" << dx << ", dy=" << dy << std::endl;
-//	std::cout << "offset.x=" << offset.x << ", offset.y=" << offset.y << std::endl;
-//	std::cout << "content.area.x=" << content.area.x << ", content.area.y=" << content.area.y << std::endl;
+	this->refresh();
 }
 
 void ScrollablePane::setOffset(int x, int y)
