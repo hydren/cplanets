0f589eb67054717d95d0942139d2df93003a6578
Carlos F. M. Faruolo <5carlosfelipe5@gmail.com>
2016-07-26 11:44:48 -0300

    Fixed Midpoint solver implementation.

--- src/physics/solvers/rk2.cpp
+++ src/physics/solvers/rk2.cpp
@@ -18,37 +18,25 @@ MidpointSolver::MidpointSolver(Universe2D& u)
 
 void MidpointSolver::step()
 {
-	const double half_timestep = timestep * 0.5;
-
 	map<Body2D*, Vector2D> k1vs, k2vs,
 							k1rs, k2rs;
 
-	map<Body2D*, Vector2D> y0, v0;
+	map<Body2D*, Vector2D> wr, wv;
+
+	derive(k1vs, k1rs);
+
 	foreach(Body2D*, body, vector<Body2D*>, universe.bodies)
 	{
-		y0[body] = body->position;
-		v0[body] = body->velocity;
+		wv[body] = body->velocity + k1vs[body] * timestep * 0.5;
+		wr[body] = body->position + k1rs[body] * timestep * 0.5;
 	}
 
-	derive(k1vs, k1rs, v0, y0);
+	derive(k2vs, k2rs, wv, wr);
 
 	foreach(Body2D*, body, vector<Body2D*>, universe.bodies)
 	{
-		Vector2D& k2v = k2vs[body];
-		Vector2D& k2r = k2rs[body];
-		const Vector2D& k1v = k1vs[body];
-		const Vector2D& k1r = k1rs[body];
-
-		k2v = body->velocity + k1v * half_timestep;
-		k2r = body->position + k1r * half_timestep;
-	}
-
-	derive(k1vs, k1rs, k2vs, k2rs);
-
-	foreach(Body2D*, body, vector<Body2D*>, universe.bodies)
-	{
-		body->velocity += k1vs[body] * timestep;
-		body->position += k1rs[body] * timestep;
+		body->velocity += k2vs[body] * timestep;
+		body->position += k2rs[body] * timestep;
 	}
 
 	timeElapsed += timestep;
