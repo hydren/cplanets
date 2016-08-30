2809bb0be255cf1ca513468755d523315157fe6a
Carlos F. M. Faruolo <5carlosfelipe5@gmail.com>
2016-08-30 16:20:16 -0300

    Adding documentation to Physics2D::ReferenceFrame

--- src/physics/physics2d.hpp
+++ src/physics/physics2d.hpp
@@ -29,17 +29,38 @@ struct Physics2D
 	/** Exchanges the planetarium universe with a copy of the given instance. This is the proper way to do it. */
 	void setUniverse(const Universe2D& universe);
 
+	/** Structure holding data about needed to compute a reference frame scheme.
+	 *  A ReferenceFrame can either be set as:
+	 *  -  The center of mass of a given set of bodies (thus, having a net velocity and total mass);
+	 *  -  A fixed point (thus having zero velocity and mass), being coined as "point-like";
+	 *  -  A moving frame, with specified start position and fixed velocity (thus having zero mass), being also coined "point-like" */
 	struct ReferenceFrame
 	{
 		friend class Physics2D;
+
+		/** Returns the current position of this reference frame. */
 		Vector2D position() const;
+
+		/** Returns the current velocity of this reference frame. */
 		Vector2D velocity() const;
+
+		/** If this reference frame is not point-like, returns the total mass of the set of bodies from which this reference frame tracks. */
 		double mass() const;
+
+		/** Returns true if this reference frame is **not** set as the center of mass of some given set of bodies. */
 		bool isPointLike() const;
 
+		/** Sets this reference frame with default values. In other words, sets itself as a static, point-like reference frame, pointing to origin (0, 0) */
 		void reset();
+
+		/** Sets this reference frame as a moving frame with specified start position and velocity. If velocity is not specified or has zero magnitude, the reference frame is a fixed point.
+		 * In both cases, this reference frame is considered "point-like" and isPointLike() should return 'true' after using this method. */
 		void set(Vector2D position, Vector2D velocity=Vector2D());
+
+		/** Sets this reference frame as the center of mass of the given set of bodies. 'reference' should be an 'n'-sized array of Body2D's pointers. */
 		void set(const Body2D** reference, unsigned n);
+
+		/** Sets this reference frame as the center of mass of the given set of bodies */
 		void set(const std::vector<Body2D*>& reference);
 
 		protected:
