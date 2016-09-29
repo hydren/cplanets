91216bca1578323d6d46e2e9c5a61ce8031cf676
Carlos F. M. Faruolo <5carlosfelipe5@gmail.com>
2016-09-29 16:07:35 -0300

    Implemented IconToogleButton. Changed IconButton implementation with IconLabel.

--- src/widgets/icon_button.cpp
+++ src/widgets/icon_button.cpp
@@ -9,46 +9,71 @@
 
 #include "widgets_util.hpp"
 
+using WidgetsExtra::IconLabel;
 using WidgetsExtra::IconButton;
+using WidgetsExtra::IconToogleButton;
 
 IconButton::IconButton(WinBase *parent, Style style, Rect bounds, Label label, SDL_Surface* icon, void (*cmd)(Button*), Id id)
 : Button(parent, style, bounds, label, cmd, id),
   icon(icon)
 {}
 
-IconButton::~IconButton()
-{
-	SDL_FreeSurface(this->icon);
-}
-
-void IconButton::pack(int height)
-{
-	if(label.render_t->text_width(label.str) < icon->w)
-	{
-		int properWidth = icon->w + 4;
-		int properHeight = height != 0? height : TTF_FontHeight(label.render_t->ttf_font) + 4;
-		widen(properWidth - tw_area.w, properHeight - tw_area.h);
-	}
-	else
-		packLabeledComponent(this, height);
-}
-
 void IconButton::draw()
 {
 	init_gui();
 	Button::draw();
-	if(icon == null) return;
+	icon.drawOn(this, static_cast<IconLabel::IconMode>(style.param2));
+}
+
+IconToogleButton::IconToogleButton(WinBase *parent, Style style, Rect bounds, Label label, SDL_Surface* icon, void (*_cmd)(CheckBox*), Id id)
+: ToogleButton(parent, style, bounds, label, cmd, id),
+  icon(icon)
+{}
+
+void IconToogleButton::draw()
+{
+	init_gui();
+	ToogleButton::draw();
+	icon.drawOn(this, static_cast<IconLabel::IconMode>(style.param2));
+}
+
+IconLabel::IconLabel(SDL_Surface* iconImage)
+: image(iconImage)
+{}
+
+IconLabel::~IconLabel()
+{
+	SDL_FreeSurface(this->image);
+}
+
+void IconLabel::drawOn(WinBase* wb, IconMode mode)
+{
+	if(image == null or wb == null) return;
 
 	SDL_Rect dstrect;
-	if(style.param2 == ICON_CENTER || style.param2 == ICON_CENTER_DOWN || style.param2 == ICON_CENTER_UP)
-		dstrect.x = (tw_area.w - icon->w)/2;
-	else if(style.param2 == ICON_RIGHT || style.param2 == ICON_RIGHT_DOWN || style.param2 == ICON_RIGHT_UP)
-		dstrect.x = tw_area.w - icon->w;
+	if(mode == ICON_CENTER || mode == ICON_CENTER_DOWN || mode == ICON_CENTER_UP)
+		dstrect.x = (wb->tw_area.w - image->w)/2;
+	else if(mode == ICON_RIGHT || mode == ICON_RIGHT_DOWN || mode == ICON_RIGHT_UP)
+		dstrect.x = wb->tw_area.w - image->w;
 
-	if(style.param2 == ICON_CENTER || style.param2 == ICON_LEFT || style.param2 == ICON_RIGHT)
-		dstrect.y = (tw_area.h - icon->h)/2;
-	else if(style.param2 == ICON_CENTER_DOWN || style.param2 == ICON_LEFT_DOWN || style.param2 == ICON_RIGHT_DOWN)
-		dstrect.y = tw_area.h - icon->h;
+	if(mode == ICON_CENTER || mode == ICON_LEFT || mode == ICON_RIGHT)
+		dstrect.y = (wb->tw_area.h - image->h)/2;
+	else if(mode == ICON_CENTER_DOWN || mode == ICON_LEFT_DOWN || mode == ICON_RIGHT_DOWN)
+		dstrect.y = wb->tw_area.h - image->h;
 
-	SDL_BlitSurface(icon, null, win, &dstrect);
+	SDL_BlitSurface(image, null, wb->win, &dstrect);
+}
+
+void IconLabel::packButton(Button* btn, int height)
+{
+	if(image == null or btn == null) return;
+
+	if(btn->label.render_t->text_width(btn->label.str) < image->w)
+	{
+		int properWidth = image->w + 4;
+		int properHeight = height != 0? height : TTF_FontHeight(btn->label.render_t->ttf_font) + 4;
+		btn->widen(properWidth - btn->tw_area.w, properHeight - btn->tw_area.h);
+	}
+	else
+		packLabeledComponent(btn, height);
 }
--- src/widgets/icon_button.hpp
+++ src/widgets/icon_button.hpp
@@ -12,18 +12,42 @@
 #include "SDL_widgets/SDL_widgets.h"
 #include "futil/general/language.hpp"
 
+#include "toogle_button.hpp"
+
 namespace WidgetsExtra
 {
-	struct IconButton extends Button
+	struct IconLabel
 	{
 		// pass one of this modes to style.param2
 		enum IconMode { ICON_CENTER, ICON_LEFT, ICON_RIGHT, ICON_CENTER_UP, ICON_CENTER_DOWN, ICON_LEFT_UP, ICON_LEFT_DOWN, ICON_RIGHT_UP, ICON_RIGHT_DOWN };
 
-		SDL_Surface* icon;
+		SDL_Surface* image;
+
+		IconLabel(SDL_Surface* iconImage);
+		virtual ~IconLabel();
+
+		virtual void drawOn(WinBase* wb, IconMode=ICON_CENTER);
+		virtual void packButton(Button* btn, int height=0);
+	};
+
+	struct IconButton extends Button
+	{
+		IconLabel icon;
+
 		IconButton(WinBase *parent, Style style, Rect bounds, Label label, SDL_Surface* icon, void (*cmd)(Button*), Id id=0);
-		virtual ~IconButton();
+		virtual ~IconButton(){}
+
 		virtual void draw();
-		virtual void pack(int height=0);
+	};
+
+	struct IconToogleButton extends ToogleButton
+	{
+		IconLabel icon;
+
+		IconToogleButton(WinBase *parent, Style style, Rect bounds, Label label, SDL_Surface* icon, void (*_cmd)(CheckBox*), Id id=0);
+		virtual ~IconToogleButton(){}
+
+		virtual void draw();
 	};
 }
 
