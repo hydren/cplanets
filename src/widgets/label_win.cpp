a8d02bca7a42ab9941bb16ab35a83a0ba93c5dc1
Carlos F. M. Faruolo <5carlosfelipe5@gmail.com>
2016-10-06 17:36:50 -0300

    Created a multi-line version of the LabelWin.

--- src/widgets/label_win.cpp
+++ src/widgets/label_win.cpp
@@ -1,9 +1,13 @@
 
 #include "label_win.hpp"
 
+#include "widgets_util.hpp"
+
 using std::string;
+using std::vector;
 
 using WidgetsExtra::LabelWin;
+using WidgetsExtra::MultiLineLabelWin;
 
 LabelWin::LabelWin(WinBase* pw, Rect bounds, string str, Id id)
 : WinBase(pw, null, bounds.x, bounds.y, (bounds.w > 0? bounds.w : draw_ttf->text_width(str.c_str())), (bounds.h > 0? bounds.h : TTF_FontHeight(draw_ttf->ttf_font)), 0, id),
@@ -23,4 +27,47 @@ void LabelWin::setTextRenderer(RenderText* txtrnd)
 {
 	this->textRenderer = txtrnd;
 	this->widen(this->tw_area.w - txtrnd->text_width(this->txt.c_str()), this->tw_area.h - TTF_FontHeight(txtrnd->ttf_font)); //fixme this overrides user-specified size
+}
+
+// ============================================ Multi line label win =====================================================================
+
+static Rect& getRectWithHeight(Rect&, unsigned, std::string&, RenderText*);
+
+MultiLineLabelWin::MultiLineLabelWin(WinBase* pw, Rect bounds, string str, unsigned padding, Id id)
+: LabelWin(pw, getRectWithHeight(bounds, padding, str, draw_ttf), str, id),
+  multiRender(draw_ttf, null, Point(), padding)
+{
+	multiRender.setText(str, bounds.w);
+}
+
+MultiLineLabelWin::~MultiLineLabelWin(){}
+
+void MultiLineLabelWin::draw()
+{
+	this->init_gui();
+	SDL_FillRect(this->win, 0, this->parent->bgcol);
+	multiRender.draw(this->win);
+}
+
+void MultiLineLabelWin::widen(int dx, int dy)
+{
+	this->widen(dx, dy);
+	this->multiRender.setText(this->txt, this->tw_area.w);
+}
+
+void MultiLineLabelWin::setTextRenderer(RenderText* txtrnd)
+{
+	LabelWin::setTextRenderer(txtrnd);
+	this->multiRender.renderText = txtrnd;
+	this->multiRender.setText(this->txt, this->tw_area.w);
+}
+
+Rect& getRectWithHeight(Rect& bounds, unsigned padding, std::string& str, RenderText* render)
+{
+	if(bounds.h == 0)
+	{
+		vector<string> tmp;
+		bounds.h = WidgetsExtra::getLineWrappedText(str, render, bounds.w - 2*padding, &tmp)->size() * TTF_FontHeight(render->ttf_font) + padding;
+	}
+	return bounds;
 }
--- src/widgets/label_win.hpp
+++ src/widgets/label_win.hpp
@@ -9,8 +9,10 @@
 #define WIDGETS_LABEL_WIN_HPP_
 
 #include <string>
+#include <vector>
 
 #include "SDL_widgets/SDL_widgets.h"
+#include "multi_line_text_renderer.hpp"
 
 #include "futil/general/language.hpp"
 
@@ -29,8 +31,22 @@ namespace WidgetsExtra
 		/** Safer than changing textRenderer directly, as this automatically adjusts size. */
 		virtual void setTextRenderer(RenderText* txtrnd);
 	};
+
+	struct MultiLineLabelWin extends LabelWin
+	{
+		MultiLineTextRenderer multiRender;
+
+		MultiLineLabelWin(WinBase *pw, Rect bounds, std::string str, unsigned padding=0, Id id=0);
+
+		virtual ~MultiLineLabelWin();
+		virtual void draw();
+
+		// pseudo-overrides WinBase::widen()
+		virtual void widen(int dx,int dy);
+
+		/** Safer than changing textRenderer directly, as this automatically adjusts size. */
+		virtual void setTextRenderer(RenderText* txtrnd);
+	};
 }
-
-
 
 #endif /* WIDGETS_LABEL_WIN_HPP_ */
