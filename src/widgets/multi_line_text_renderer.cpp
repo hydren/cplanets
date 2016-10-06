99cfe451aec78c68859c3818d02eb4065cdfcec0
Carlos F. M. Faruolo <5carlosfelipe5@gmail.com>
2016-10-06 17:32:37 -0300

    Created MultiLineTextRenderer to help drawing multi-line texts.

---
+++ src/widgets/multi_line_text_renderer.cpp
@@ -0,0 +1,65 @@
+/*
+ * multi_line_text_renderer.cpp
+ *
+ *  Created on: 6 de out de 2016
+ *      Author: carlosfaruolo
+ */
+
+#include "multi_line_text_renderer.hpp"
+
+#include "widgets_util.hpp"
+
+using WidgetsExtra::MultiLineTextRenderer;
+using std::string;
+using std::vector;
+
+MultiLineTextRenderer::MultiLineTextRenderer(RenderText* renderText, SDL_Surface* surface, Point position, unsigned padding)
+: renderText(renderText), surface(surface), position(position), padding(padding)
+{}
+
+void MultiLineTextRenderer::draw()
+{
+	this->draw(this->surface, this->position, this->padding);
+}
+
+void MultiLineTextRenderer::draw(SDL_Surface* surface)
+{
+	this->draw(surface, this->position, this->padding);
+}
+
+void MultiLineTextRenderer::draw(SDL_Surface* surface, Point position, unsigned padding)
+{
+	const int lineHeight = TTF_FontHeight(this->renderText->ttf_font);
+	int lineNumber = 0;
+	foreach(string&, line, vector<string>, lines)
+	{
+		renderText->draw_string(surface, line.c_str(), Point(position.x + padding, position.y + padding + lineHeight * lineNumber++));
+	}
+}
+
+unsigned MultiLineTextRenderer::getLineCount() const
+{
+	return this->lines.size();
+}
+
+unsigned MultiLineTextRenderer::getTextHeight() const
+{
+	return this->lines.size() * TTF_FontHeight(this->renderText->ttf_font) + this->padding;
+}
+
+unsigned MultiLineTextRenderer::getTextHeight(unsigned padding) const
+{
+	return this->lines.size() * TTF_FontHeight(this->renderText->ttf_font) + padding;
+}
+
+void MultiLineTextRenderer::breakLines(Uint16 maxWidth)
+{
+	this->lines.clear();
+	WidgetsExtra::getLineWrappedText(this->fullText, this->renderText, maxWidth - 2*this->padding, &this->lines);
+}
+
+void MultiLineTextRenderer::setText(std::string text, Uint16 maxWidth)
+{
+	this->fullText = text;
+	this->breakLines(maxWidth);
+}
---
+++ src/widgets/multi_line_text_renderer.hpp
@@ -0,0 +1,48 @@
+/*
+ * multi_line_text_renderer.hpp
+ *
+ *  Created on: 6 de out de 2016
+ *      Author: carlosfaruolo
+ */
+
+#ifndef WIDGETS_MULTI_LINE_TEXT_RENDERER_HPP_
+#define WIDGETS_MULTI_LINE_TEXT_RENDERER_HPP_
+#include <ciso646>
+
+#include <string>
+#include <vector>
+
+#include "SDL_widgets/SDL_widgets.h"
+
+#include "futil/general/language.hpp"
+
+namespace WidgetsExtra
+{
+	struct MultiLineTextRenderer
+	{
+		RenderText* renderText;
+		SDL_Surface* surface;
+		Point position;
+		int padding;
+
+		MultiLineTextRenderer(RenderText* renderText=null, SDL_Surface* surface=null, Point position=Point(), unsigned padding=0);
+
+		void draw();
+		void draw(SDL_Surface* surface);
+		void draw(SDL_Surface* surface, Point position, unsigned padding=0);
+
+		unsigned getLineCount() const;
+
+		unsigned getTextHeight() const;
+		unsigned getTextHeight(unsigned padding) const;
+
+		void breakLines(Uint16 maxWidth);
+		void setText(std::string text, Uint16 maxWidth);
+
+		protected:
+		std::vector<std::string> lines;
+		std::string fullText;
+	};
+}
+
+#endif /* WIDGETS_MULTI_LINE_TEXT_RENDERER_HPP_ */
